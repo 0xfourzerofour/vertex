e0d105594243071ab6a28ff426b2872de25b5af4
Joshua Pauline <joshpauline@gmail.com>
2022-04-25 17:36:27 +1000

    test

--- internal/graphql/graphql.go
+++ internal/graphql/graphql.go
@@ -1,9 +1,49 @@
 package graphql
 
-func GetQueryService() {
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/vektah/gqlparser/v2/ast"
+	"github.com/vektah/gqlparser/v2/parser"
+)
+
+type HttpQuery struct {
+	Query string `json:"query"`
+}
+
+func GetQueryService(body *[]byte) {
+
+	parseQuery(body)
 
 }
 
-func parseQuery() {
+func parseQuery(body *[]byte) {
+
+	hq := HttpQuery{}
+
+	err := json.Unmarshal(*body, &hq)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	queryAst := ast.Source{
+		Input: hq.Query,
+	}
+
+	as, err := parser.ParseQuery(&queryAst)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	for _, operation := range as.Operations {
+		// Name query/mutation name
+
+		//Directive query or mutation
+
+		log.Printf("%+v", operation.SelectionSet[0])
+	}
 
 }
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
+	"govertex/internal/graphql"
 	"log"
 
 	"github.com/valyala/fasthttp"
-	"github.com/vektah/gqlparser/v2/ast"
-	"github.com/vektah/gqlparser/v2/parser"
 
 	proxy "github.com/yeqown/fasthttp-reverse-proxy/v2"
 )
@@ -17,41 +15,11 @@ var (
 	proxyServer2 = proxy.NewReverseProxy("localhost:3001")
 )
 
-type HttpQuery struct {
-	Query string `json:"query"`
-}
-
-// ProxyHandler ... fasthttp.RequestHandler func
 func ProxyHandler(ctx *fasthttp.RequestCtx) {
-	// parse gql and send to correct service
 
 	body := ctx.Request.Body()
 
-	hq := HttpQuery{}
-
-	err := json.Unmarshal(body, &hq)
-
-	if err != nil {
-		log.Print(err)
-	}
-
-	queryAst := ast.Source{
-		Input: hq.Query,
-	}
-
-	as, err := parser.ParseQuery(&queryAst)
-
-	if err != nil {
-		log.Print(err)
-	}
-
-	for _, operation := range as.Operations {
-		// Name query/mutation name
-
-		//Directive query or mutation
-
-		log.Printf("%+v", operation.SelectionSet[0])
-	}
+	graphql.GetQueryService(&body)
 
 	proxyServer.ServeHTTP(ctx)
 
@@ -61,6 +29,8 @@ func main() {
 	// Load services from config
 
 	// build proxies
+
+	//introspection must be on to get schema docs
 
 	if err := fasthttp.ListenAndServe("localhost:3000", ProxyHandler); err != nil {
 		log.Fatal(err)
